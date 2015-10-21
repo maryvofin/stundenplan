6cb1084ed22a5de9d70f951bb625d940bb1d1a1c
maryvofin <mark.freiberg@maryvofin.de>
2015-10-21 15:14:21 +0200

    Ein paar Warnungen abgearbeitet

--- app/src/main/java/de/maryvofin/stundenplan/app/database/IProfileAdapter.java
+++ app/src/main/java/de/maryvofin/stundenplan/app/database/IProfileAdapter.java
@@ -1,7 +1,6 @@
 package de.maryvofin.stundenplan.app.database;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 
@@ -13,11 +12,10 @@ import com.mikepenz.materialdrawer.holder.StringHolder;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
-/**
- * Created by mark on 20.10.2015.
- */
+
 public class IProfileAdapter implements IProfile<IProfileAdapter> {
 
     Profile profile;
@@ -117,18 +115,10 @@ public class IProfileAdapter implements IProfile<IProfileAdapter> {
         ColorGenerator generator = ColorGenerator.MATERIAL;
 
         for(Profile p: Database.getInstance().getProfiles().getProfiles()) {
-            list.add(new IProfileAdapter(p).withIcon(TextDrawable.builder().buildRect(p.getName().substring(0,1).toUpperCase(), generator.getColor(p.getName()))));
+            list.add(new IProfileAdapter(p).withIcon(TextDrawable.builder().buildRect(p.getName().substring(0,1).toUpperCase(Locale.getDefault()), generator.getColor(p.getName()))));
         }
 
         return list;
-    }
-
-    public static Profile getProfileFromIProfile(IProfile iProfile) {
-        int id = iProfile.getIdentifier();
-        for(Profile p: Database.getInstance().getProfiles().getProfiles()) {
-            if(p.getUuid().hashCode() == id) return p;
-        }
-        return null;
     }
 
     public Profile getProfile() {
