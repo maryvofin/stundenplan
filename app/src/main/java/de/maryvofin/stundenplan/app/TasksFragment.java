96c44d84fb7fd00d9468c873187914932a5f7d9d
maryvofin <mark.freiberg@maryvofin.de>
2015-12-04 13:26:37 +0100

    Design für AddTaskActivity abgeschlossen

---
+++ app/src/main/java/de/maryvofin/stundenplan/app/TasksFragment.java
@@ -0,0 +1,22 @@
+package de.maryvofin.stundenplan.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TasksFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+
+        return view;
+    }
+
+}
